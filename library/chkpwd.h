9582200fcf7bfa2cdfd78ed3ae1de25b61e0cc79
Bryan Jones <unix.chkpwd@gmail.com>
2024-04-29 22:49:57 -0400

    changes

---
+++ library/chkpwd.h
@@ -0,0 +1,9 @@
+// Colemak home row mods
+#define CK_A LGUI_T(KC_A)
+#define CK_R LALT_T(KC_R)
+#define CK_S LCTL_T(KC_S)
+#define CK_T LSFT_T(KC_T)
+#define CK_N RSFT_T(KC_N)
+#define CK_E RCTL_T(KC_E)
+#define CK_I LALT_T(KC_I)
+#define CK_O RGUI_T(KC_O)
