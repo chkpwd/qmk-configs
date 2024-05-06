c656bbf13c641abe03ad4f86c10ecefd1865b30f
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-06 18:58:37 -0400

    add fn layer

--- keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
@@ -79,11 +79,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_UTILS] = LAYOUT_split_3x6_3(  // utilities layer
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, _______, _______, _______, _______, XXXXXXX,                      XXXXXXX, XXXXXXX, DT_DOWN, DT_UP, KC_MS_R, XXXXXXX,
+      XXXXXXX, KC_F5,  KC_F6,  KC_F7,  KC_F8, XXXXXXX,                          XXXXXXX, XXXXXXX, DT_DOWN,   DT_UP, KC_MS_R, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, KC_F1,  KC_F2,  KC_F3,  KC_F4, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          XXXXXXX, XXXXXXX, DT_PRNT,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
