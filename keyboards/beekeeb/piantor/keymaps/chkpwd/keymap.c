0e6bc012e43ef7fac62c2b7f90d6f5b8be5976fc
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-07 07:50:11 -0400

    indenting

--- keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
@@ -54,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUMSYM] = LAYOUT_split_3x6_3(  // number + symbols layer
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_BSPC,  KC_PMNS,  KC_P7,  KC_P8,  KC_P9, KC_PSLS,                         KC_ASTR, KC_LPRN, KC_RPRN, KC_AT, KC_EXLM, KC_AMPR,
+      KC_BSPC,  KC_PMNS,  KC_P7,  KC_P8,  KC_P9, KC_PSLS,                        KC_ASTR, KC_LPRN, KC_RPRN, KC_AT, KC_EXLM,   KC_AMPR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_EQL,   KC_PPLS,  KC_P4,  KC_P5,  KC_P6, KC_PAST,                         KC_TILD, KC_LBRC, KC_RBRC, KC_DLR, KC_HASH, KC_BSLS,
+      KC_EQL,   KC_PPLS,  KC_P4,  KC_P5,  KC_P6, KC_PAST,                        KC_TILD, KC_LBRC, KC_RBRC, KC_DLR, KC_HASH,  KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_PENT,  KC_MINS,  KC_P1,  KC_P2,  KC_P3,   KC_P0,                         KC_PIPE, KC_LCBR, KC_RCBR, KC_CIRC, KC_PERC, KC_UNDS,
+      KC_PENT,  KC_MINS,  KC_P1,  KC_P2,  KC_P3,   KC_P0,                        KC_PIPE, KC_LCBR, KC_RCBR, KC_CIRC, KC_PERC, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                      _______,    KC_PCMM, KC_PDOT,    XXXXXXX, XXXXXXX, XXXXXXX
+                                        _______,  KC_PCMM, KC_PDOT,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
