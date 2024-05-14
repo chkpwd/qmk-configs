ceb9ea5e56d95b6d3d95e868baeae0a9fc4ffe3e
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-14 02:00:20 -0400

    change position of ^!

--- keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
        KC_ENT,    KC_0,  KC_P4,  KC_P5,  KC_P6, KC_PPLS,                       KC_TILD, KC_PIPE,  KC_DLR, KC_LBRC, KC_RBRC, KC_HASH,
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
-      _______, KC_PDOT,  KC_P1,  KC_P2,  KC_P3, KC_UNDS,                        KC_EQL, KC_CIRC, KC_PERC, KC_LCBR, KC_RCBR, KC_EXLM,
+      _______, KC_PDOT,  KC_P1,  KC_P2,  KC_P3, KC_UNDS,                        KC_EQL, KC_EXLM, KC_PERC, KC_LCBR, KC_RCBR, KC_CIRC,
   //|--------------------------------------------------------------|  |--------------------------------------------------------------|
                                          XXXXXXX,  XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
