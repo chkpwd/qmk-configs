fe6fb15b78a5f404a18ac943334f9ff15f96fa84
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-29 12:30:10 -0400

    switched thumb keys

--- keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
       KC_RBRC,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,   KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
   //|--------------------------------------------------------------|  |--------------------------------------------------------------|
-                                          LTMED,   LTNAV,  KC_LSFT,    LTUTILS,   LTNUM, LTPNT
+                                           LTMED,   LTNAV,  KC_LSFT,   LTPNT,     LTNUM, LTUTILS
                                       //`--------------------------'  `--------------------------'
   ),
 
