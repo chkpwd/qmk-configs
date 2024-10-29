3818bc9869dfd7338c9d15608a419424e3d45d9e
Bryan Jones <unix.chkpwd@gmail.com>
2024-10-29 17:08:31 -0400

    add tap dance for media keys

--- keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/chkpwd/keymap.c
@@ -11,19 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
       KC_RBRC,   KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,   KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
   //|--------------------------------------------------------------|  |--------------------------------------------------------------|
-                                           TD_ESC_CAPS,   LTNAV,  HYPER,  LTPNT,     LTNUM, LTUTILS
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [_MED] = LAYOUT_split_3x6_3(  // media layer
-  //,-----------------------------------------------------.                  ,-----------------------------------------------------.
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|-----------------------------------------------------|                  |-----------------------------------------------------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPRV, XXXXXXX,
-  //|-----------------------------------------------------|                  |-----------------------------------------------------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------------------------------------------------------------|  |------------------------------------------------------------|
-                                          KC_MPLY, _______, _______,    KC_MPLY, KC_MSTP, KC_MUTE
+                                           TD(_MED),   LTNAV,  HYPER,  LTPNT,     LTNUM, LTUTILS
                                       //`--------------------------'  `--------------------------'
   ),
 
--- users/chkpwd/chkpwd.c
+++ users/chkpwd/chkpwd.c
@@ -3,10 +3,50 @@
 #include "chkpwd.h"
 #include "features/achordion.h"
 
-// Tap Dance definitions
+// Determine the current tap dance state
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) return TD_DOUBLE_TAP;
+    else return TD_UNKNOWN;
+}
+
+// Initialize tap structure associated with example tap dance key
+static td_tap_t ql_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// Functions that control what our tap dance key does
+void ql_finished(tap_dance_state_t *state, void *user_data) {
+    ql_tap_state.state = cur_dance(state);
+    switch (ql_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_MPLY);
+            break;
+        case TD_SINGLE_HOLD:
+            tap_code(KC_MUTE);
+            break;
+        case TD_DOUBLE_TAP:
+            tap_code(KC_MNXT);
+            break;
+        default:
+            break;
+    }
+}
+
+void ql_reset(tap_dance_state_t *state, void *user_data) {
+    // If the key was held down and now is released then switch off the layer
+    if (ql_tap_state.state == TD_SINGLE_HOLD) {
+        tap_code(KC_MUTE);
+    }
+    ql_tap_state.state = TD_NONE;
+}
+
+// Associate our tap dance key with its functionality
 tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+    [_MED] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
 };
 
 /* Shared functions across all keyboards */
